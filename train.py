c753fa8c03acbdc5a2a65d68abdba0423f1a40f9
Alyssa Lee <alyssalee0329@gmail.com>
2025-10-03 18:35:57 -0400

    1

--- train.py
+++ train.py
@@ -7,8 +7,20 @@ from torch.utils.data import DataLoader
 
 
 class BasicFeedForward(nn.Module):
-    pass
+    def __init__(self):
+        super(BasicFeedForward, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=256)
+        self.fc3 = nn.Linear(in_features=256, out_features=128)
+        self.fc4 = nn.Linear(in_features=128, out_features=10)
 
+    def forward(self, x):
+        x = x.view(-1, 28 * 28) #Flattening the input
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x) #not activated (log likelihood)
+        return x
 
 if __name__ == "__main__":
     if torch.cuda.is_available():
@@ -20,3 +32,14 @@ if __name__ == "__main__":
 
     print(f"Using device: {device}")
 
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean = (0.1307,), std = (0.3081,))
+    ])
+
+    train_dataset = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST(train_dataset, batch_size=128, shuffle=True)
+
+    model = BasicFeedForward().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    
